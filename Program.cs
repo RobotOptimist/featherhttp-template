3b78e5649365751ce8301017ea19eb844d6bf5f2
James MacIvor <jmacivor@steemer.com>
2020-01-03 14:24:51 -0500

    created empty featherhttp project and template.json

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+    }
+}
